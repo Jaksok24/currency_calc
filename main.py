b6470e1a5313f6a4c499d909dd11c13322d60ce8
Jaksok24 <157789383+Jaksok24@users.noreply.github.com>
2024-08-30 13:01:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
 import requests
 
 url = "https://api.nbp.pl/api/exchangerates/tables/A/"
-
 response = requests.get(url)
+
+def Calc():
+    print('---KALKULATOR WALUT---\n')
+    while True:
+        choice = input('Wybierz walute\n1.Dolar amerykański\n2.Euro\n3.Wyjdź\n')
+        match choice:
+            case '1':
+                num = int(input('Podaj kwote do przeliczenia: '))
+                print(f"\n{num} USD kosztuje {round(num*usd, 2)} PLN\n")
+            case '2':
+                num = int(input('Podaj kwote do przeliczenia: '))
+                print(f"\n{num} EUR kosztuje {round(num*eur, 2)} PLN\n")
+            case '3':
+                break
+            case _:
+                print('Wprowadź odpowiednią opcję\n')
 
 if response.status_code == 200:
     data = response.json()
     for rate in data[0]['rates']:
         if rate['code'] == 'USD':
-            print(f"Kurs dolara (USD): {round(rate['mid'],2)} PLN")
-            
+            usd = rate['mid']
+        if rate['code'] == 'EUR':
+            eur = rate['mid']
 else:
     print(f"Błąd: {response.status_code}")
+
+Calc()
